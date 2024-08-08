0d79f56aa27b6da65f634bd053fb1ee8d6972b4c
vikasM30 <mishraviki.mishra@gmail.com>
2024-08-09 00:05:33 +0530

    dynamic programming

--- Algo/misc/Dynamic_programming/coin_problem.js
+++ Algo/misc/Dynamic_programming/coin_problem.js
@@ -1,6 +1,10 @@
-// what is the minimum number of coins combinations required to produce a sum "n"
+// CASE 1: what is the minimum number of coins combinations required to produce a sum "n"
 
 const minimum_coin_combination = (sum, coinsList) => {
     if (sum == 0) return 0
     
 }
+
+
+
+// CASE 2: given sum "n", in how many ways we can form this sum from given sets of coin?
