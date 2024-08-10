d54a29cdeb1c328e427c0b3c5bd15daede65276a
vikasM30 <mishraviki.mishra@gmail.com>
2024-08-10 22:01:20 +0530

    coin problem update

--- Algo/misc/Dynamic_programming/coin_problem.js
+++ Algo/misc/Dynamic_programming/coin_problem.js
@@ -2,16 +2,22 @@
 
 // CASE 2: given sum "n", in how many ways we can form this sum from given sets of coin?
 
+// For greedy approach, we require to itirate the coins array every time again and again and then also we get wrong answer,
+// So, we need to apply dynamic programming
+// here we see how many minimum amount we need to make sum 1,2,3,4,5... (sum + 1)
+
 const minimum_coin_combination = (sum, coinsList) => {
+    if (sum < 1) return 0;
     const dp = Array(sum + 1).fill(Infinity);
     dp[0] = 0;
+    noOfCoinsRequired = {}
     for (let i = 0; i < coinsList.length; i++) {
         for (let j = coinsList[i]; j <= sum; j++) {
-            dp[j] = Math.min(dp[j], dp[j - coinsList[i]] + 1);
+            noOfCoinsRequired[j] = dp[j] = Math.min(dp[j], dp[j - coinsList[i]] + 1);
         }
     }
     // CASE 1
-    const noOfCombination = dp[sum] === Infinity ? -1 : dp[sum];
+    const noOfCombination = noOfCoinsRequired[sum] === Infinity ? -1 : noOfCoinsRequired[sum];
     // CASE 2
     let i = sum;
     const result = [];
@@ -25,7 +31,7 @@ const minimum_coin_combination = (sum, coinsList) => {
       }
     }
    
-    return {noOfCombination, coins: result.join(' ')};
+    return {noOfCombination, coins: result.join(' '), noOfCoinsRequired};
 }
 
 console.log(minimum_coin_combination(11,[1,2,5]))
