7aed0f351d73225662484a7be64bf4761ad05719
vikasM30 <mishraviki.mishra@gmail.com>
2024-08-10 19:19:31 +0530

    coin problem

--- Algo/misc/Dynamic_programming/coin_problem.js
+++ Algo/misc/Dynamic_programming/coin_problem.js
@@ -1,10 +1,31 @@
 // CASE 1: what is the minimum number of coins combinations required to produce a sum "n"
 
+// CASE 2: given sum "n", in how many ways we can form this sum from given sets of coin?
+
 const minimum_coin_combination = (sum, coinsList) => {
-    if (sum == 0) return 0
-    
+    const dp = Array(sum + 1).fill(Infinity);
+    dp[0] = 0;
+    for (let i = 0; i < coinsList.length; i++) {
+        for (let j = coinsList[i]; j <= sum; j++) {
+            dp[j] = Math.min(dp[j], dp[j - coinsList[i]] + 1);
+        }
+    }
+    // CASE 1
+    const noOfCombination = dp[sum] === Infinity ? -1 : dp[sum];
+    // CASE 2
+    let i = sum;
+    const result = [];
+    while (sum > 0) {
+      for (let j = 0; j < coinsList.length; j++) {
+        if (sum >= coinsList[j] && dp[sum - coinsList[j]] === dp[sum] - 1) {
+          result.push(coinsList[j]);
+          sum -= coinsList[j];
+          break;
+        }
+      }
+    }
+   
+    return {noOfCombination, coins: result.join(' ')};
 }
 
-
-
-// CASE 2: given sum "n", in how many ways we can form this sum from given sets of coin?
+console.log(minimum_coin_combination(11,[1,2,5]))
